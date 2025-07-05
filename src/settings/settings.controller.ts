9d0a99a085b5fb211a21c14613c1d63f7ffd9c1f
PatrixInTheMatrix <p.perrin@diwidi.net>
2025-07-05 10:26:30 +0200

    feat: implement CORS, WebSocket broadcast and settings update API

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -1,4 +1,6 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
+import { SettingsService } from './settings.service';
+import { SettingsGateway } from 'src/settings.gateway';
 
 interface PraxisSettings {
   backgroundColor: string;
@@ -16,6 +18,10 @@ export class SettingsController {
     menu: [],
   };
 
+  constructor(
+    private readonly gateway: SettingsGateway
+  ) { }
+
   @Get()
   getSettings(): PraxisSettings {
     return this.settings;
@@ -24,6 +30,7 @@ export class SettingsController {
   @Post()
   saveSettings(@Body() dto: PraxisSettings): { success: boolean } {
     this.settings = dto;
+    this.gateway.broadcastSettingsUpdate(); // 🔁 alle Clients benachrichtigen
     return { success: true };
   }
 }
