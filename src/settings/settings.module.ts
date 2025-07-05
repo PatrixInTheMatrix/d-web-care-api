cf18435521c30257731d78510bf6d505a15be052
PatrixInTheMatrix <p.perrin@diwidi.net>
2025-07-05 11:21:40 +0200

    fix(settings): register SettingsModel with MongooseModule

--- src/settings/settings.module.ts
+++ src/settings/settings.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Settings, SettingsSchema } from './settings.schema';
-import { SettingsGateway } from './settings.gateway';
 import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
+import { SettingsGateway } from './settings.gateway';
+import { Settings, SettingsSchema } from './../settings/settings.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Settings.name, schema: SettingsSchema }])
+    MongooseModule.forFeature([{ name: Settings.name, schema: SettingsSchema }]),
   ],
   providers: [SettingsService, SettingsGateway],
   controllers: [SettingsController],
